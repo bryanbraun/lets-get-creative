44d9d561d7d362e52c43b9d5ec7543539cce5ab7
Bryan Braun <bbraun7@gmail.com>
2023-08-12 21:35:43 -0400

    feat: add rss link to the document <head>

--- src/components/TldrawWrapper.jsx
+++ src/components/TldrawWrapper.jsx
@@ -1,8 +1,4 @@
 import { Tldraw } from '@tldraw/tldraw'
-
-// Eventually, I think I'll need to switch from importing editor.css
-// and ui.css to tldraw.css, per the recent release notes:
-// https://github.com/tldraw/tldraw/releases/tag/v2.0.0-alpha.13
 import '@tldraw/tldraw/tldraw.css'
 
 export default function TldrawWrapper () {
